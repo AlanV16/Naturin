3f2e3538842355b82657abc3b227373f3a188ba5
Alan Valles <139717394+AlanV16@users.noreply.github.com>
2025-06-06 01:22:32 -0500

    06-06-2025

---
+++ version 2.0.0/apps/gamification/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'gamification'
+
+urlpatterns = [
+    # Rutas para gamificación
+    # path('logros/', views.achievements, name='achievements'),
+    # path('recompensas/', views.rewards, name='rewards'),
+]
