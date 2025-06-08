0c45daaf7f24fc5b9652423ce75e70e86bdfa2f6
Ader <157672072+KaliJhomen@users.noreply.github.com>
2025-06-08 16:18:27 -0500

    añadido el url para explore

--- version 2.0.0/base/urls.py
+++ version 2.0.0/base/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('especies/', views.species_list, name='species_list'),
     path('especies/<int:species_id>/', views.species_detail, name='species_detail'),
     path('categoria/<str:category_name>/', views.category_detail, name='category'),  
-    path('explorar/', views.explorar, name='explorar'),
-    path('juegos/', views.juegos, name='juegos'),
+    path('explorar/', views.explore, name='explore'),
+    path('explorar/<slug:slug>/', views.explore_detail, name='explore_detail'),
 ]
